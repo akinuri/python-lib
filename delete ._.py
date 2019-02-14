419848d92a81668b98895493a6c2bb9460b85c0b
Noreh AD <akinoreh@gmail.com>
2019-02-14 13:45:42 +0300

    .

---
+++ delete ._.py
@@ -0,0 +1,35 @@
+import glob, os, sys
+
+from pprint import pprint
+
+os.system("title " + "Deleting ._ files")
+
+# ========================= FOLDER DROP
+
+input_path = None
+
+if len(sys.argv) > 1:
+    input_path = sys.argv[1]
+else:
+    input("Drop a folder on me.")
+    sys.exit()
+
+if not os.path.isdir(input_path):
+    input("You must enter a folder.")
+    sys.exit()
+
+# ========================= DELETE
+
+i = 0
+    
+for root, dirs, files in os.walk(input_path):
+    for filename in files:
+        if filename.startswith("._"):
+            path = os.path.join(root, filename)
+            os.remove(path)
+            print(path)
+            i += 1
+
+print("\nFiles Deleted: " + str(i))
+
+input("\nPress Enter key to exit...")
